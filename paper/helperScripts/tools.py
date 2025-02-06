b7998cc84193c1f7d0498a56567052b6e44ea50d
kins912 <kins912@gmail.com>
2025-02-06 16:39:07 -0500

    paper updates

--- paper/helperScripts/tools.py
+++ paper/helperScripts/tools.py
@@ -47,6 +47,20 @@ gene_cols = [
 # genes_to_exclude = ['BMP4', 'ENSMUSG00000071361', 'ITGA8', 'KIT', 'NANOG', 'ROR2', 'SOX2']
 genes_to_exclude = ['BMP4', 'ENSMUSG00000071361', 'ITGA8', 'KIT', 'NANOG', 'ROR2', 'SOX2']
 
+regev_cell_cycle_genes = [
+                            # S Phase
+                           'MCM5','PCNA','TYMS','FEN1','MCM2','MCM4','RRM1','UNG','GINS2','MCM6','CDCA7','DTL','PRIM1','UHRF1',
+                           'MLF1IP','HELLS','RFC2','RPA2','NASP','RAD51AP1','GMNN','WDR76','SLBP','CCNE2','UBR7','POLD3','MSH2',
+                           'ATAD2','RAD51','RRM2','CDC45','CDC6','EXO1','TIPIN','DSCC1','BLM','CASP8AP2','USP1','CLSPN','POLA1',
+                           'CHAF1B','BRIP1','E2F8',
+                           
+                           # G2/M Phase
+                           'HMGB2','CDK1','NUSAP1','UBE2C','BIRC5','TPX2','TOP2A','NDC80','CKS2','NUF2',
+                           'CKS1B','MKI67','TMPO','CENPF','TACC3','FAM64A','SMC4','CCNB2','CKAP2L','CKAP2','AURKB','BUB1','KIF11',
+                           'ANP32E','TUBB4B','GTSE1','KIF20B','HJURP','CDCA3','HN1','CDC20','TTK','CDC25C','KIF2C','RANGAP1','NCAPD2',
+                           'DLGAP5','CDCA2','CDCA8','ECT2','KIF23','HMMR','AURKA','PSRC1','ANLN','LBR','CKAP5','CENPE','CTCF','NEK2',
+                           'G2E3','GAS2L3','CBX5','CENPA']
+
 roi_file_paths = {
 
     'roi_1':{'segmentation_file':'/Users/grantkinsler/RajLab Dropbox/Grant Kinsler/SpatialBarcodes/ImagingData/2024-05-21_mouseexp_expression/projects/roi_1/segmentations/nuclei_20240604_nuclei.tiff',
