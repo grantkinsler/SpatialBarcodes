dfc755ce1971f41c9dac880b6e9f5794f80265c7
kins912 <kins912@gmail.com>
2025-02-10 11:27:58 -0500

    re-organization

---
+++ extractionScripts/1_process_to_SGobject_and_call_barcodes.py
@@ -0,0 +1,184 @@
+import numpy as np
+# import cv2
+import tifffile as tiff
+from shapely.geometry import Polygon, mapping, shape, box
+from skimage import io
+from skimage.measure import label, regionprops
+# import matplotlib.pyplot as plt
+from rasterio import features
+import scanpy as sc
+import pandas as pd
+import geopandas as gpd
+
+import pickle
+import scipy
+
+from SGanalysis.SGobject import SGobject 
+
+
+from sklearn.cluster import AgglomerativeClustering as AggCluster
+
+import sys
+import os
+
+tools_path = '../helperScripts/tools.py'
+
+sys.path.append(os.path.dirname(os.path.expanduser(tools_path)))
+import tools
+
+
+partially_processed = False
+cutoff = 3
+barcode_cols = ['bc_{:03d}'.format(i) for i in range(1,97)]
+
+from SGanalysis.SGobject import SGobject
+
+for roi_name,files in tools.roi_file_paths.items():
+
+    print(f'Processing {roi_name}...')
+
+    segmentation_file = files['segmentation_file']
+    spots_file = files['spots_file']
+    out_path = files['out_path']
+    
+    if not partially_processed:
+        # Create an instance of SGobject
+        sg_obj = SGobject()
+
+        # Convert a TIFF image to polygons and store them in a GeoDataFrame
+        print("Running mask_to_objects...")
+        sg_obj.mask_to_objects(segmentation_file)
+
+        sg_obj.load_points(spots_file)
+
+        sg_obj.dilate_objects(10)
+
+        ## associate spots with segmentation
+
+        sg_obj.create_cell_gene_table()
+
+        # with open(f'{out_path}/sg_object_dilate10_20240718.pkl', 'wb') as f:
+        #     pickle.dump(sg_obj, f)
+            
+        matrix = sg_obj.get_cell_gene_table_df()
+
+        if matrix.index.name == 'object_id':
+            matrix = matrix.reset_index()
+        matrix['object_id'] = [str(int(x)) for x in matrix['object_id']]
+        matrix.set_index('object_id',inplace=True)
+
+        sg_obj.gdf['object_id'] = [str(int(x)) for x in sg_obj.gdf['object_id']]
+        sg_obj.gdf.set_index('object_id',inplace=True)
+
+        sg_obj.gdf['nucleus_centroid'] = sg_obj.gdf['nucleus'].centroid.values
+        sg_obj.gdf['center_x'] = sg_obj.gdf['nucleus'].centroid.x.values
+        sg_obj.gdf['center_y'] = sg_obj.gdf['nucleus'].centroid.y.values
+        sg_obj.gdf['area'] = sg_obj.gdf['nucleus_dilated'].area.values
+
+        sg_obj.gdf
+
+        barcode_cols = ['bc_{:03d}'.format(i) for i in range(1,97)]
+
+        with open(f'{out_path}/sg_object_dilate10_20240718.pkl', 'wb') as f:
+            pickle.dump(sg_obj, f)
+
+
+        ## traditional barcode calling (using cutoff of 3)
+        cell_barcodes = {}
+        # matrix.set_index('object_id',   inplace=True)
+        # df.set_index('object_id')
+
+        df = matrix
+        df['cell_id'] = df.index
+
+        for cell_id in df['cell_id']:
+            this_cell = df[df['cell_id']==cell_id]
+            cell_barcodes[cell_id] = []
+
+            for bc in barcode_cols:
+                if this_cell[bc].values[0] >= cutoff:
+                    cell_barcodes[cell_id].append(bc)
+
+        df['called_barcodes'] = cell_barcodes.values()
+        df['n_called_barcodes'] = [len(bc_set) for bc_set in cell_barcodes.values()]
+        df['barcode_names'] = ['-'.join(sorted(bc_set)) for bc_set in cell_barcodes.values()]
+
+        df = pd.merge(df,sg_obj.gdf,how='left',left_index=True,right_index=True)
+
+        df.to_csv(f'{out_path}/cell_by_gene_matrix_dilate10_20240718_withbarcodes_atleast{cutoff}.csv')
+    else:
+#    	with open(f'{out_path}/sg_object_dilate10_20240718.pkl', 'rb') as f:
+#		sg_obj = pickle.load(f)
+		
+	    df = pd.read_csv(f'{out_path}/cell_by_gene_matrix_dilate10_20240718_withbarcodes_atleast{cutoff}.csv')
+
+
+    total_bc_threshold = 10
+
+    # include cells with at least 10 barcode spots
+    has_bcs = df[df[barcode_cols].sum(axis=1) >= 10]
+    matrix = has_bcs[barcode_cols]
+
+    print('Clustering barcodes...')
+    print('Calculating distance matrix...')
+
+    matrix_norm = matrix.div(matrix.sum(axis=1), axis=0)
+    braycurtis_dist = scipy.spatial.distance.pdist(matrix_norm,metric='braycurtis')
+    
+    print('Performing clustering...')
+
+    if 'dish' in roi_name:
+        threshold = 0.2
+    else:
+        threshold = 0.4
+    # cluster = AggCluster(distance_threshold=threshold,n_clusters=None,linkage='average',metric='precomputed').fit(scipy.spatial.distance.squareform(braycurtis_dist))
+    cluster = AggCluster(distance_threshold=threshold,n_clusters=None,linkage='average',affinity='precomputed').fit(scipy.spatial.distance.squareform(braycurtis_dist))
+
+
+    gene_cols = [col for col in df.columns if 'bc_' not in col and col not in ['cell_id','called_barcodes','n_called_barcodes','barcode_names','area','center_x','center_y','nucleus','nucleus_centroid','nucleus_dilated']]    
+
+    adata_genes = sc.AnnData(df[gene_cols])
+
+
+    cols= ['cell_id','n_called_barcodes','barcode_names','called_barcodes','area','center_x','center_y']
+    # for col in :
+    #     adata_genes.obs[col] = df[col]
+
+    adata_genes.obs = df[cols]
+
+
+    cluster_name_cutoff = 3
+
+    obj_clusters = {obj_id:clus for obj_id,clus in zip(matrix.index,cluster.labels_)}
+
+    cluster_objects = {}
+    cluster_barcode_names = {}
+    cluster_found_barcodes = {}
+    cluster_n_found_barcodes = {}
+
+    for clu in np.unique(cluster.labels_):
+        cluster_objects[clu] = matrix.index[cluster.labels_ == clu]
+
+        avg_bc_counts = matrix[cluster.labels_ == clu].median(axis=0)
+        # print(avg_bc_counts)
+        # break
+
+        found_bcs = []
+
+        for bc in barcode_cols:
+            if avg_bc_counts[bc] > cutoff:
+                found_bcs.append(bc)
+
+#        print(clu,found_bcs)
+
+        cluster_found_barcodes[clu] = found_bcs
+        cluster_n_found_barcodes[clu] = len(found_bcs)
+
+        cluster_barcode_names[clu] = '-'.join(sorted(found_bcs))
+
+    adata_genes.obs['bc_cluster'] = [obj_clusters[obj_id] if obj_id in obj_clusters else np.nan for obj_id in adata_genes.obs.index]
+    adata_genes.obs['bc_cluster_n_bcs'] = [cluster_n_found_barcodes[obj_clusters[obj_id]] if obj_id in obj_clusters else np.nan for obj_id in adata_genes.obs.index]
+    adata_genes.obs['bc_cluster_found_bcs'] = [cluster_found_barcodes[obj_clusters[obj_id]] if obj_id in obj_clusters else np.nan for obj_id in adata_genes.obs.index]
+    adata_genes.obs['bc_cluster_bc_names'] = [cluster_barcode_names[obj_clusters[obj_id]] if obj_id in obj_clusters else np.nan for obj_id in adata_genes.obs.index]
+
+    adata_genes.obs.to_csv(f'{out_path}/cell_by_gene_matrix_dilate10_20240718_withbarcodes_clustering_{total_bc_threshold}bcs_{threshold}thresh.csv')
---
+++ extractionScripts/2_erode_tumor.py
@@ -0,0 +1,200 @@
+import scipy.spatial.distance as sci_dist
+from PIL import Image
+from scipy.spatial import ConvexHull
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import PolygonSelector
+import matplotlib
+# matplotlib.use('QtAgg')
+from matplotlib.path import Path
+import cv2
+from matplotlib.colors import hsv_to_rgb
+from skimage.measure import regionprops, label
+Image.MAX_IMAGE_PIXELS = None
+np.random.seed(0)
+import sys
+
+tools_path = '../helperScripts/tools.py'
+
+sys.path.append(os.path.dirname(os.path.expanduser(tools_path)))
+import tools as tools
+
+data_folder = tools.roi_file_paths['roi_2']['out_path']
+temp_out_path = data_folder + '/../processedData/erosion_analysis'
+
+final_out_path = '../extractedData/in_vivo/'
+# output_folder = data_folder + '/processedData/erosion_analysis'
+# cell_by_gene_path = data_folder + '/exports/cell_by_gene_matrix_20240606_10px_withbarcodes_atleast3.csv'
+cell_by_gene_path = data_folder + '/cell_by_gene_matrix_dilate10_20240718_withbarcodes_atleast3.csv'
+# pd.read_csv(f'{tools.roi_file_paths[roi_of_interest]["out_path"]}/
+# selected_mask = data_folder +  '/processedData/erosion_analysis/selected_mask.png'
+selected_mask = final_out_path + 'selected_mask.png'
+transcripts_path = data_folder + '/decode_20240604.csv'
+
+
+
+
+
+class ErosionAnalysis:
+    def __init__(self):
+        self.load_data()
+        self.mask = np.zeros((48115, 60616), dtype=np.uint8)
+        self.MAX_DIM = 20000
+        self.kernel_size = 100   # Increase this value to make each erosion more substantial
+        self.scaling_factor = 1  # Default to no scaling
+        self.erosion_iterations = 25
+        self.pixel2nm = 107.11 # every pixel is 107.11 nm
+    def load_data(self):
+        self.cell_by_gene = pd.read_csv(cell_by_gene_path)
+        self.original_centers = self.cell_by_gene[['center_x', 'center_y']].values.astype(float)
+        self.centers = self.original_centers.copy()
+        self.areas = self.cell_by_gene['area'].values
+        self.cell_id = self.cell_by_gene['cell_id'].values
+        self.cell_by_gene = pd.read_csv(cell_by_gene_path)
+
+    def resize_image_and_coordinates(self, image):
+        height, width = image.shape
+        if max(height, width) > self.MAX_DIM:
+            self.scaling_factor = self.MAX_DIM / max(height, width)
+            image_pil = Image.fromarray(image)
+            image_pil = image_pil.resize((int(width * self.scaling_factor), int(height * self.scaling_factor)), Image.NEAREST)
+            self.centers *= self.scaling_factor
+            return np.array(image_pil)
+        self.scaling_factor = 1  # No scaling
+        return image
+
+    def check_mask(self):
+        mask_file = temp_out_path +f'/num_iterations_{self.erosion_iterations}' + '/selected_mask.png'
+        # print(mask_file)
+        if os.path.exists(mask_file):
+            print(f"Loading existing mask from {mask_file}")
+            mask = Image.open(mask_file)
+            mask = mask.convert('L')
+            mask = np.array(mask)
+            self.mask = self.resize_image_and_coordinates(mask)
+        else:
+            print("Creating a new mask")
+            self.create_mask()
+
+    def create_mask(self):
+        fig, ax = plt.subplots(figsize=(10, 10))
+        scatter = ax.scatter(self.centers[:, 0], self.centers[:, 1], s=15, c='red', alpha=0.5)
+        plt.title('Select points to create a mask')
+        plt.xlabel('X Coordinate')
+        plt.ylabel('Y Coordinate')
+
+        def onselect(verts):
+            path = Path(verts)
+            self.mask.fill(0)
+            cv2.fillPoly(self.mask, [np.array(verts, np.int32)], 255)
+
+            self.mask = self.resize_image_and_coordinates(self.mask)
+            mask_image = Image.fromarray(self.mask)
+            mask_image.save(temp_out_path+f'/num_iterations_{self.erosion_iterations}'  + '/selected_mask.png')
+
+            plt.figure(figsize=(10, 10))
+            plt.imshow(self.mask, cmap='gray')
+            plt.title('Initial Mask')
+            plt.xlabel('X Coordinate')
+            plt.ylabel('Y Coordinate')
+            # plt.show()
+
+        polygon_selector = PolygonSelector(ax, onselect)
+        # plt.show()
+    def calculate_ring_width(self, mask1, mask2):
+        # Subtract mask2 from mask1 to get the ring
+        ring_mask = mask1 & ~mask2
+        labeled_ring = label(ring_mask)
+        labeled_outer = label(mask1)
+    
+        # Find the properties of the labeled regions
+        region_ring = regionprops(labeled_ring)[0]
+        region_mask = regionprops(labeled_outer)[0]
+    
+        # Area and perimeter of the ring
+        ring_area = region_ring.area
+        ring_perimeter = region_mask.perimeter
+    
+        # Correct the area and perimeter back to original dimensions
+        corrected_ring_area = ring_area / (self.scaling_factor ** 2)
+        corrected_ring_perimeter = ring_perimeter / self.scaling_factor
+    
+        # Calculate the average width of the ring based on the corrected perimeter
+        average_ring_width = corrected_ring_area / corrected_ring_perimeter
+    
+        # Convert to micrometers
+        average_ring_width_um = average_ring_width * self.pixel2nm / 1000
+    
+        return average_ring_width_um
+    def erode_and_save(self):
+        
+        eroded_coordinates = []
+        ring_widths = []
+        initial_mask = self.mask.copy()
+        previous_mask = self.mask.copy()
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (self.kernel_size, self.kernel_size))
+
+        for i in range(self.erosion_iterations):
+            print(f"Erosion iteration {i + 1}")
+            print(f"Sum of previous mask before erosion: {np.sum(previous_mask)}")
+            self.mask = cv2.erode(self.mask, kernel)
+            print(f"Sum of current mask after erosion: {np.sum(self.mask)}")
+
+            difference_mask = cv2.subtract(previous_mask, self.mask)
+            width = self.calculate_ring_width(previous_mask, self.mask)
+            ring_widths.append({'Ring Width (um)': width })
+            eroded_coords = []
+
+            for j, (x, y) in zip(self.cell_id,self.centers):
+                if int(y) < difference_mask.shape[0] and int(x) < difference_mask.shape[1] and difference_mask[int(y), int(x)] > 0:
+                    eroded_coords.append((j, x / self.scaling_factor, y / self.scaling_factor))
+            
+            eroded_coordinates.append(eroded_coords)
+            if self.scaling_factor != 1:
+                eroded_mask_image = Image.fromarray(cv2.resize(self.mask, (int(self.mask.shape[1] / self.scaling_factor), int(self.mask.shape[0] / self.scaling_factor)), interpolation=cv2.INTER_NEAREST))
+            else:
+                eroded_mask_image = Image.fromarray(self.mask)
+            eroded_mask_image.save(temp_out_path+f'/num_iterations_{self.erosion_iterations}' + f'/eroded_mask_{i + 1}.png')
+            eroded_df = pd.DataFrame(eroded_coords, columns=['cell_id', 'X', 'Y'])
+            eroded_df.to_csv(temp_out_path+f'/num_iterations_{self.erosion_iterations}'  + f'/eroded_coords_{i + 1}_.csv', index=False)
+
+            previous_mask = self.mask.copy()
+            cells_in_ring = eroded_df['cell_id'].values
+            self.cell_by_gene.loc[self.cell_by_gene['cell_id'].isin(cells_in_ring), 'Ring'] = i
+        # Convert to DataFrame and save as CSV
+        df_ring_widths = pd.DataFrame(ring_widths)
+        output_csv_path = os.path.join(temp_out_path,f'num_iterations_{self.erosion_iterations}', 'ring_widths.csv')
+        self.cell_by_gene.to_csv(os.path.join(final_out_path, 'cell_by_gene_with_rings.csv'))
+        df_ring_widths.to_csv(output_csv_path, index=False)
+        print('Mean ring width:',np.mean(df_ring_widths['Ring Width (um)']))
+        # Generate dynamic colors for plotting
+        hsv_colors = [(i / self.erosion_iterations, 1, 1) for i in range(self.erosion_iterations)]
+        rgb_colors = hsv_to_rgb(hsv_colors)
+
+        # Display the final eroded mask
+        if self.scaling_factor != 1:
+            initial_mask_resized = cv2.resize(initial_mask, (int(initial_mask.shape[1] / self.scaling_factor), int(initial_mask.shape[0] / self.scaling_factor)), interpolation=cv2.INTER_NEAREST)
+        else:
+            initial_mask_resized = initial_mask
+
+        plt.figure(figsize=(10, 10))
+        plt.imshow(initial_mask_resized, cmap='gray')
+        for i, coords in enumerate(eroded_coordinates):
+            if coords:
+                _, x, y = zip(*coords)
+                plt.scatter(x, y, s=10, c=[rgb_colors[i]], label=f'Iteration {i+1}', alpha=0.6)
+        plt.title('Final Eroded Mask with Cell Locations')
+        plt.xlabel('X Coordinate')
+        plt.ylabel('Y Coordinate')
+        plt.legend()
+        plt.savefig(temp_out_path +f'/num_iterations_{self.erosion_iterations}' + '/final_mask.jpg')
+        # plt.show()
+
+
+if __name__ == "__main__":
+    analysis = ErosionAnalysis()
+    analysis.check_mask()
+    analysis.erode_and_save()
